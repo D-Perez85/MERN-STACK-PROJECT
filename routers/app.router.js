c26d530a08be6092d0e90b567501f57f4b21d290
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-10-09 23:35:46 -0300

    ISSUE  #14: Nueva config de Router => Carrito

--- routers/app.router.js
+++ routers/app.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const productosRoutes = require('./productos/productos');
+const carritoRoutes = require('./carrito/carrito'); 
 
 const router = express.Router();
 router.use('/productos', productosRoutes);
+router.use('/carrito', carritoRoutes)
 
 module.exports = router;
